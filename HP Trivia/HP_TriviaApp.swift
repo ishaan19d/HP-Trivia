f15e3a1f26f97b63e981365a1e2e249a236a7fac
ishaan19d <ishaandas848@gmail.com>
2024-06-21 20:26:22 +0530

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Ishaan Das on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
